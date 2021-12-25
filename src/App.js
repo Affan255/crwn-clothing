4da5cd28627483821c3487c61ae07bba2622cb55
Affan Ahmad <affana@versa-networks.com>
2021-12-25 14:16:07 +0530

    update MenutItem with withRouter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import './App.css'
 import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom'
+
+const Hats = () => {
+  return <h1>HATS PAGE</h1>
+}
 
 function App() {
-  return <HomePage />
+
+ return(
+   <div>
+     <Route exact path="/" component={HomePage} />
+     <Route exact path="/hats" component={Hats} />
+   </div>
+ ) 
+ 
 }
 
 export default App
